8e4c6a589c9b7d95e20e43abd2093aefb50bfde1
Claire Fahy <zeebear21@gmail.com>
2019-02-26 16:48:37 -0300

    add oauth to user model, initializers/devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[linkedin]
 
   has_many :projects, dependent: :destroy
   has_many :project_members
