449074755e89bc811b7e5967fcbba4324b9018cf
Claire Fahy <zeebear21@gmail.com>
2019-02-26 18:57:54 -0300

    fixed field in migrate

--- db/migrate/20190226203722_add_oauth_to_users.rb
+++ db/migrate/20190226203722_add_oauth_to_users.rb
@@ -6,7 +6,7 @@ class AddOauthToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :current_sign_in_ip, :inet
     add_column :users, :last_sign_in_ip, :inet
     add_column :users, :provider, :string
-    add_column :users, :id, :string, unique: true
+    add_column :users, :uid, :string, unique: true
     add_column :users, :access_token, :string
     add_column :users, :firstName, :string
     add_column :users, :lastName, :string
