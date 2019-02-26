6f053c3624fffe1e0624c5fb4ac5908c2c732418
Matias Coto <maticoto20@gmail.com>
2019-02-26 17:25:06 -0300

    stlye to the index cards

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,3 +7,16 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 end
+
+  # def reviews
+  #   reviews = []
+  #   self.bookings.each do |booking|
+
+  #     reviews << booking.review if booking.review
+  #   end
+  #   return reviews
+  # end
+
+  # def rating
+  #   reviews.map { |review| review.rating }.sum / reviews.count.to_f
+  # end
--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,12 @@ class Project < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :reviews, through: :orders
   validates :name, presence: true
+
+  def rating
+    ratings = []
+    self.reviews.each do |review|
+      ratings << review.rating
+    end
+    return ratings.empty? ? 0 : ratings.sum / ratings.count
+  end
 end
