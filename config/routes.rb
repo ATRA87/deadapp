cd6144b3cbb1ad3ff54e9161136a77a8618f4863
Claire Fahy <zeebear21@gmail.com>
2019-02-26 17:13:16 -0300

    add linkedin method

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   end
   resources :teams, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'authorizations' }
 
 end
