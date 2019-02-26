f22665b94918e502193a44fc55e41feb690ed532
Claire Fahy <zeebear21@gmail.com>
2019-02-26 19:21:44 -0300

    commented out alternative methods in auth controller

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -5,18 +5,18 @@ class AuthorizationsController < ApplicationController
     sign_in_and_redirect @user, event: :authentication
   end
 
-  def callback
-    begin
-      oauth = OauthService.new(request.env['omniauth.auth'])
-      if oauth_account = oauth.create_oauth_account!
-          ...
-          redirect_to Config.provider_login_path
-      end
-      rescue => e
-        flash[:alert] = "There was an error while trying to authenticate your account."
-        redirect_to register_path
-    end
-  end
+  # def callback
+  #   begin
+  #     oauth = OauthService.new(request.env['omniauth.auth'])
+  #     if oauth_account = oauth.create_oauth_account!
+  #         ...
+  #         redirect_to Config.provider_login_path
+  #     end
+  #     rescue => e
+  #       flash[:alert] = "There was an error while trying to authenticate your account."
+  #       redirect_to register_path
+  #   end
+  # end
 
   def failure
     flash[:alert] = "There was an error while trying to authenticate your account."
