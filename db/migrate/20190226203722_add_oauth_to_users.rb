e8b8eda004068d949b528c56c7d34dd8e181e393
Claire Fahy <zeebear21@gmail.com>
2019-02-27 11:41:30 -0300

    normal login and OAuth with LinkedIn both work

--- db/migrate/20190226203722_add_oauth_to_users.rb
+++ db/migrate/20190226203722_add_oauth_to_users.rb
@@ -1,6 +1,6 @@
 class AddOauthToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :sign_in_count, :integer
+    add_column :users, :sign_in_count, :integer, default: 0, null: false
     add_column :users, :current_sign_in_at, :datetime
     add_column :users, :last_sign_in_at, :datetime
     add_column :users, :current_sign_in_ip, :inet
@@ -8,8 +8,8 @@ class AddOauthToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :provider, :string
     add_column :users, :uid, :string, unique: true
     add_column :users, :access_token, :string
-    add_column :users, :firstName, :string
-    add_column :users, :lastName, :string
-    add_column :users, :profilePicture, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :profile_picture, :string
   end
 end
