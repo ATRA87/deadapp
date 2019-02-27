f5554c8c49bcff79116efa2861218c31b1890449
Matias Coto <maticoto20@gmail.com>
2019-02-27 15:38:37 -0300

    added default pic to proyects with no assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
+  end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
