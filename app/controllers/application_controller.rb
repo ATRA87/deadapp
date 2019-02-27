49c237e0e45ba25a7b1a682483ddd4c4cd677213
Claire Fahy <zeebear21@gmail.com>
2019-02-27 15:14:02 -0300

    fix schema and missing 'end' in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,12 +20,12 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username first_name last_name])
-
   end
 end
