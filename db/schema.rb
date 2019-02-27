7d8761c1ac39df98a7a7047947dccdfa18e5b5c2
Claire Fahy <zeebear21@gmail.com>
2019-02-27 13:33:10 -0300

    set default user image in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_213439) do
     t.string "access_token"
     t.string "first_name"
     t.string "last_name"
-    t.string "profile_picture"
+    t.string "profile_picture", default: "unofficial_hand.jpg", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
