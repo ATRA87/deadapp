f5e8522b221a476896e3314ebe4650fbf55cc487
Alfonso Quintana <quintana.f.alfonso@gmail.com>
2019-02-27 14:38:31 -0300

    added text to the login view page, and relocated the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_163359) do
+ActiveRecord::Schema.define(version: 2019_02_26_211054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_163359) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
