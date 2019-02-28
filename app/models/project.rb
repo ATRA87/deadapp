78ca67a9160d264e74c6b807235ca7da4288174a
Claire Fahy <zeebear21@gmail.com>
2019-02-28 19:15:19 -0300

    maybe this time seeds work?

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
   has_many :project_members, dependent: :destroy
   has_many :users, through: :project_members
   has_many :customizations, dependent: :destroy
-  has_many :project_details, dependent: :destroy
+  has_many :project_assets, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :reviews, through: :orders
   validates :name, presence: true
