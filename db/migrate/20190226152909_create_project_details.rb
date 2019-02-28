b2d05b959751899befab18aca67e5b8d60ade6ba
Claire Fahy <zeebear21@gmail.com>
2019-02-28 16:15:25 -0300

    project_details description field now customize, fix plural table names

--- db/migrate/20190226152909_create_project_details.rb
+++ db/migrate/20190226152909_create_project_details.rb
@@ -2,7 +2,7 @@ class CreateProjectDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :project_details do |t|
       t.references :project, foreign_key: true
-      t.string :description
+      t.string :customize
 
       t.timestamps
     end
