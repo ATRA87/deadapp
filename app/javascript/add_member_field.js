eb0c49f99e94f4b8c3ebf85b8e735d75cc73511e
Matias Coto <maticoto20@gmail.com>
2019-03-01 18:12:41 -0300

    asd

--- app/javascript/add_member_field.js
+++ app/javascript/add_member_field.js
@@ -1,16 +1,41 @@
-const btn = document.querySelector('#add-btn');
+const addBtn = document.querySelector('#add-btn');
 const membersForm = document.querySelector('#members');
 const memberField = document.querySelector('#project_user_ids');
 const options = memberField.querySelectorAll('option');
 
-options.forEach(option => {
-  console.log(option);
-  option.addEventListener('click', (event) => {
-    membersForm.insertAdjacentHTML('beforeend', `<select class="form-control select optional" name="project[user_ids][]" id="project_user_ids">`+memberField.innerHTML+`</select>`);
-    console.log(hola);
+addBtn.addEventListener('click', (event) => {
+  membersForm.insertAdjacentHTML('beforeend', `<div class="selector-field" style="display: flex; align-items: center;">
+        <select class="form-control select optional" name="project[user_ids][]" id="project_user_ids"><option value=""></option>`+memberField.innerHTML+`</select>
+        <div id="delete-btn" style="margin-left: 10px;"><i class="far fa-trash-alt" id="delete-btn"></i></div>
+      </div>`);
+});
+
+const deleteBtns = document.querySelectorAll('#delete-btn');
+
+deleteBtns.forEach(deleteBtn => {
+  deleteBtn.addEventListener('click', (event) => {
+    deleteBtn.parentNode.remove();
+    // console.log(deleteBtn.parentNode);
   });
 });
-btn.addEventListener('click', (event) => {
-  membersForm.insertAdjacentHTML('beforeend', `<select class="form-control select optional" name="project[user_ids][]" id="project_user_ids"> <option value=""></option>`+memberField.innerHTML+`</select>`);
-  console.log(memberField.innerHTML);
-});
+
+var modal = document.getElementById('form-modal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("edit-btn");
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
