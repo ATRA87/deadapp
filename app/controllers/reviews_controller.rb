60d2b11fc02b508a5d51080b60634659a8a8200a
Claire Fahy <zeebear21@gmail.com>
2019-03-04 11:02:44 -0300

    save changes to reviews controller, TBC

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,31 @@
 class ReviewsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[new create]
+  before_action :set_order
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new
+    @review.communication_rating = params['communication_rating']
+    @review.quality_rating = params['quality_rating']
+    @review.delivery_on_time_rating = params['delivery_on_time_rating']
+
+    orders = []
+    project = @review.order.project
+
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[order_id])
+    @review.order = @order
+    authorize @review
+  end
+
+  def review_params
+    params.require(review).permit(:communication_rating, :quality_rating, :delivery_on_time_rating, :content)
   end
 end
