40576fdcbc5e32171418968b534f0e3c1f67f893
Claire Fahy <zeebear21@gmail.com>
2019-03-04 11:53:58 -0300

    review has one project, review create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,10 @@
 class ReviewsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[new create]
-  before_action :set_order
+  before_action :set_review_order
 
   def new
     @review = Review.new
+    @order = Order.find(params[order_id])
   end
 
   def create
@@ -14,13 +15,28 @@ class ReviewsController < ApplicationController
 
     orders = []
     project = @review.order.project
+    Order.where(user: current_user).where(project: project) do |o|
+      orders << o if o.review.nil?
+    end
+    @order = orders.first
+    authorize @order unless @order.nil?
 
+    if @review.save
+      respond_to do |format|
+        format.html { redirect_to @order}
+        format.js # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render project } # or 'project/show' or 'projects/show'? cf corental
+        format.js # <-- idem
+      end
+    end
   end
 
   private
 
-  def set_order
-    @order = Order.find(params[order_id])
+  def set_review_order
     @review.order = @order
     authorize @review
   end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :order
   has_one :user, through: :order
+  has_one :project, through: :order
   validates :rating, presence: true
 end
