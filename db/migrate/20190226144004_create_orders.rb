5031aa456cdeb56fb7b3c7b00ed2d974d2b5d271
Matias Coto <maticoto20@gmail.com>
2019-03-04 10:19:22 -0300

    added dev and client state to order

--- db/migrate/20190226144004_create_orders.rb
+++ db/migrate/20190226144004_create_orders.rb
@@ -3,10 +3,12 @@ class CreateOrders < ActiveRecord::Migration[5.2]
     create_table :orders do |t|
       t.references :user, foreign_key: true
       t.references :project, foreign_key: true
-      t.integer :state, default: 0
+      t.integer :dev_state, default: 0
+      t.integer :client_state, default: 0
+      t.integer :estimated_time, default: 0
+      t.date :due_date
       t.monetize :amount, currency: { present: true }
       t.json :payment
-
 
       t.timestamps
     end
