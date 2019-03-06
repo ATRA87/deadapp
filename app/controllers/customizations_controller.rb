6e4e08cafb549abb3c2ed98693f553bc6a673419
Claire Fahy <zeebear21@gmail.com>
2019-03-06 19:55:46 -0300

    playing with forms, not done

--- app/controllers/customizations_controller.rb
+++ app/controllers/customizations_controller.rb
@@ -1,8 +1,19 @@
 class CustomizationsController < ApplicationController
+  before_action :set_project, only: %i[new create]
+
   def new
+    @customization = Customization.new
+    @customization.project = @project
+    authorize @customization
   end
 
   def create
+    @customization = Customization.new(customization_params)
+    @customization.project = @project
+    authorize @customization
+    return redirect_to @project if @customization.save
+
+    render :new
   end
 
   def edit
@@ -11,6 +22,18 @@ class CustomizationsController < ApplicationController
   def update
   end
 
-  def delete
+  def destroy
+    @customization = Customization.find(params[:project_id])
+    @customization.destroy
+  end
+
+  private
+
+  def customization_params
+    params.require(:customization).permit(:description)
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
   end
 end
