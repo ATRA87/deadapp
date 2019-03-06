d34d74b8aac5c405025ef57d7a9b59d3351014ff
Matias Coto <maticoto20@gmail.com>
2019-03-06 09:27:56 -0300

    now dropdown is on owners name

--- app/javascript/add_member_field.js
+++ app/javascript/add_member_field.js
@@ -2,46 +2,45 @@ const addBtn = document.querySelector('#add-btn');
 
 if (addBtn) {
   const membersForm = document.querySelector('#members');
-const memberField = document.querySelector('#project_user_ids');
- const options = memberField.querySelectorAll('option');
+  const memberField = document.querySelector('#project_user_ids');
+  const options = memberField.querySelectorAll('option');
 
-
-
-addBtn.addEventListener('click', (event) => {
-  membersForm.insertAdjacentHTML('beforeend', `<div class="selector-field" style="display: flex; align-items: center;">
-        <select class="form-control select optional" name="project[user_ids][]" id="project_user_ids"><option value=""></option>`+memberField.innerHTML+`</select>
-        <div id="delete-btn" style="margin-left: 10px;"><i class="far fa-trash-alt" id="delete-btn"></i></div>
-      </div>`);
-});
-
-const deleteBtns = document.querySelectorAll('#delete-btn');
-
-deleteBtns.forEach(deleteBtn => {
-  deleteBtn.addEventListener('click', (event) => {
-    deleteBtn.parentNode.remove();
-    // console.log(deleteBtn.parentNode);
+  addBtn.addEventListener('click', (event) => {
+    membersForm.insertAdjacentHTML('beforeend', `
+      <div class="selector-field" style="display: flex; align-items: center;">
+      <select class="form-control select optional" name="project[user_ids][]" id="project_user_ids"><option value=""></option>`+memberField.innerHTML+`</select>
+      <div id="delete-btn" style="margin-left: 10px;"><i class="far fa-trash-alt" id="delete-btn"></i></div>
+      </div>`
+    );
   });
-});
+
+  const deleteBtns = document.querySelectorAll('#delete-btn');
+
+  deleteBtns.forEach(deleteBtn => {
+    deleteBtn.addEventListener('click', (event) => {
+      deleteBtn.parentNode.remove();
+      // console.log(deleteBtn.parentNode);
+    });
+  });
+
+  var modal = document.getElementById('form-modal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("edit-btn");
 
 
-var modal = document.getElementById('form-modal');
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
 
-// Get the button that opens the modal
-var btn = document.getElementById("edit-btn");
-
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = "block";
   }
-}
+
+  // When the user clicks on <span> (x), close the modal
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
