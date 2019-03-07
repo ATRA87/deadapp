d9fac1da2ceb57152239d2a7ce7cacc8a90e9f27
Matias Coto <maticoto20@gmail.com>
2019-03-07 09:31:59 -0300

    added button to expand description in show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,6 @@ import "../add_member_field.js"
 import "../chats"
 import { starit } from '../plugins/mati_stars';
 import { initStripe } from '../plugins/pay_btn';
+import { expand } from '../plugins/expand_description';
 
 starit ();
