bf80d733666680961fadf709cd8f0d22fe5d0aa6
Matias Coto <maticoto20@gmail.com>
2019-03-07 11:09:56 -0300

    changed  design from chat and size of chat bubbles and font, made scroll to bottom by default

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,6 @@ import "../add_member_field.js"
 import "../chats"
 import { starit } from '../plugins/mati_stars';
 import { initStripe } from '../plugins/pay_btn';
+import { scroll } from '../plugins/chat_scroller';
 
 starit ();
